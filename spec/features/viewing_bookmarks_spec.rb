0f612564b7929cba330b649b48442e67a97328a1
Amy Jordan <amy_jordan@live.co.uk>
2019-03-25 16:44:40 +0000

    Connected to PSQL database using PG gem

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,6 +1,8 @@
 feature 'view bookmarks' do
   scenario 'user visits /bookmarks and can see their bookmarks' do
     visit('/bookmarks')
-    expect(page).to have_content('https://www.google.com', 'https://makers.tech/')
+    expect(page).to have_content("http://www.makersacademy.com")
+    expect(page).to have_content("http://www.google.com")
+    expect(page).to have_content("http://www.destroyallsoftware.com")
   end
 end
--- spec/unit/bookmark_spec.rb
+++ spec/unit/bookmark_spec.rb
@@ -4,9 +4,9 @@ describe Bookmark do
 
   it 'returns a list of URLs' do
     bookmarks = Bookmark.all
-    expect(bookmarks).to include("https://www.google.com")
-    expect(bookmarks).to include("https://makers.tech/")
+    expect(bookmarks).to include("http://www.makersacademy.com")
+    expect(bookmarks).to include("http://www.google.com")
+    expect(bookmarks).to include("http://www.destroyallsoftware.com")
   end
 
 end
-
