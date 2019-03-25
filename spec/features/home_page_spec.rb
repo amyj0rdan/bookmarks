04f250e089f3aad538c95ef071b7cc2b86ed46ed
Amy Jordan <amy_jordan@live.co.uk>
2019-03-25 14:52:44 +0000

    User sees simple welcome page

---
+++ spec/features/home_page_spec.rb
@@ -0,0 +1,6 @@
+feature 'user sees welcome page' do
+  scenario 'user opens website and sees welcome page' do
+    visit ('/')
+    expect(page).to have_content('Welcome to your Bookmark Manager')
+  end
+end
