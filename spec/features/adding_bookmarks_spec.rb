fe6a583805ba9e875125c8e195a8f366fd770c4e
Amy Jordan <amy_jordan@live.co.uk>
2019-03-29 16:10:32 +0000

    Created a happy path for the user to follow by putting an add button to the homepage, tests written and passing nicely. snowman

--- spec/features/adding_bookmarks_spec.rb
+++ spec/features/adding_bookmarks_spec.rb
@@ -1,6 +1,7 @@
 feature 'can add a bookmark' do
   scenario 'user adds a new bookmark' do
-    visit('/bookmarks/add')
+    visit('/bookmarks')
+    click_button 'Add bookmark'
     fill_in 'url', with: 'http://makers.tech/'
     fill_in 'title', with: 'Makers'
     click_button('Add')
